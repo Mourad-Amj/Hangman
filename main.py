ac1211b8483e944b539be5c967ecd2aad2636552
Mourad-Amj <mourad.amjahed@outlook.com>
2023-05-10 15:58:20 +0200

    corrected version

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 #from directory.fileName import className
 from utils.game import Hangman
+
+Hangman().start_game()
+    # if input ("another one Y/N").lower() != 'y':
--- utils/game.py
+++ utils/game.py
@@ -1,54 +1,51 @@
 import random
 
 class Hangman:
-
-    lives=5
-    turn_count=1
-    error_count=0
     possible_words = ['becode', 'learning', 'mathematics', 'sessions']
 
     def __init__(self) :
         """
         Constructor method that initializes the attributes of the Hangman class.
         """
+        self.error_count= 0
+        self.lives= 5
+        self.turn_count = 1
         self.word_to_find = list(random.choice(self.possible_words))
         # initializes the correctly guessed letters list with the appropriate number of underscores
         self.correctly_guessed_letters= ["_"]*len(self.word_to_find)
         self.wrongly_guessed_letters=[]
 
-    def verification(self, guess):
+    def verification(self, guess:str):
+
         """
         Method that checks if the given letter is in the word to be found.
         If the letter is in the word, it updates the correctly guessed letters list.
         If the letter is not in the word, it updates the wrongly guessed letters list.
         A boolean indicating whether the guess was correct or not.
         """
-        if (guess in self.word_to_find):
-            for letter in range(len(self.word_to_find)):
-                if self.word_to_find[letter] == guess:
-                    self.correctly_guessed_letters[letter] = guess
-            return True
+        if guess in self.word_to_find:
+            for i, letter in enumerate(self.word_to_find):
+                if letter == guess:
+                    self.correctly_guessed_letters[i] = guess
+            print("  Good guess ")
         else:
             self.wrongly_guessed_letters.append(guess)
             self.lives -= 1
             self.error_count +=1
-            return False
+            print("  Bad guess ")
 
     def play(self):
         """
         Method that gets user input for a letter call "guess", and calls the verification method to check if the guess is correct.
         A Bool indicating whether the guess was good or bad.
-        """       
+        """
         guess=input("Enter one letter : ").lower()
-        if guess.isalpha() is False:
+        if not guess.isalpha():
             return print("No special characters allowed! Only enter a letter, please : ")
-        elif len(guess)>1:
+        elif len(guess) > 1:
             return print("No more than one characters! Only enter a letter, please : ")
-        elif self.verification(guess) == True:
-            return print("  Good guess ")
-        else:
-            return print("  Bad guess ")
-
+        self.verification(guess)
+            
     def start_game(self):
         """
         Method that starts the game and runs a loop until the game is over.
@@ -57,9 +54,9 @@ class Hangman:
         print(" This is the word to find : ")
         print(self.correctly_guessed_letters)
         self.print_hangman()
-        while(self.lives != 0 and "_" in self.correctly_guessed_letters):
+
+        while self.lives != 0 and "_" in self.correctly_guessed_letters:
             self.play()
-            
             print(f"  Turn :  {self.turn_count}")
             print(f"  Life :  {self.lives}")
             print(f"  Error count :  {self.error_count}")
@@ -68,6 +65,7 @@ class Hangman:
             print("***** New turn ! ****")
             print(f"  Turn :  {self.turn_count}")
             print(self.correctly_guessed_letters)
+
         if self.lives != 0:
             self.well_played()
         else:
@@ -76,14 +74,14 @@ class Hangman:
     def game_over(self):
         """
         Method that prints a "Game over" message.
-        """        
+        """
         print("*** GAME OVER . . . ***")
 
     def well_played(self):
         """
         Method that prints a success message with the word found and the number of turns and errors.
         """
-        print(f"You found the word: '{''.join(self.word_to_find)}' in {self.turn_count} turns with {self.error_count} errors!")
+        print(f"You found the word: '{''.join(self.word_to_find)} ' in {self.turn_count} turns with {self.error_count} errors!")
         print(" *** Well played !! ***")
 
     def print_hangman(self):
@@ -94,5 +92,7 @@ class Hangman:
         for line in drawing_hangman:
             print(line)
 
-player = Hangman()
-player.start_game()
+if __name__=="__main__":
+    hangman = Hangman()
+    hangman.start_game()
+
